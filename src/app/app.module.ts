6fcd15d16b295b560638d59e1641b8d1630c9566
Alex Ricardo <alexp>
2017-11-16 14:37:32 -0200

    Inserção da estrutura para menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { NguiMenuModule } from '@ngui/menu';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { GravatarComponent } from './gravatar/gravatar.component';
 import { GravatarService } from './gravatar/gravatar.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GravatarComponent
+    GravatarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    NguiMenuModule
   ],
   providers: [GravatarService],
   bootstrap: [AppComponent]
